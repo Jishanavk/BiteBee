695c10938e1ca11ca8a6408fada21e1fc55f86ad
Jishana <jishanavk38@gmail.com>
2026-06-03 10:50:39 +0530

    final static files setup

--- BiteBee/settings.py
+++ BiteBee/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
